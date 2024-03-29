4cb9cd8e4b0f2f5d6250966b3012ca4fb435641d
Songminseok <songms0909@khu.ac.kr>
2024-03-29 14:56:58 +0900

    1주차 (9933.py 코드 수정)

--- ssongms/1/9933.py
+++ ssongms/1/9933.py
@@ -8,16 +8,8 @@ isRepeat = True; # 반복을 더 수행할지를 위한 플래그
 
 for password in passwordsList:
     if isRepeat : # 플래그가 True인 경우에만 반복문 수행
-        stack = [] # 스택 정의
-        queue = [] # 큐 정의
-        for idx in range(len(password)):
-            stack.append(password[idx]) # 각 password에서 한글자씩 stack에 push 
-        for i in range(len(password)):
-            queue.append(stack.pop()) # stack으로부터 pop(가장 위 요소부터 빼냄)해서 queue에 저장
-            
-        reversePassword = ''.join(queue) # 리스트를 문자열로 변환. 예를 들어 ['a','b','c'] => 'abc'
-        # 이 상태에서 reversePassword는 각 password의 역순 (abc -> cba)
-
+        reversePassword = password[::-1] # 문자열을 뒤집어 reversePassword에 저장
+        
         for comparePassword in passwordsList: 
             if(comparePassword == reversePassword): # password가 뒤집은 비밀번호와 같은지 검사
                 resultLength = len(reversePassword)
