5744a43e5e17ccda43a86e0a8757b4c2241224eb
parkyoungwoo <snake010807@khu.ac.kr>
2024-04-01 17:54:24 +0900

    1주차

---
+++ Park-YW/1/9933.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+vector<string> numbers;
+int amuont;
+
+int main()
+{
+	cin >> amount;
+	for (int index = 0; index < amount - 1; index++)
+	{
+		cin << numbers[index];
+	}
+
+	for (int index = 0; index < amount; index++)
+	{
+		for (int j = index; index < amount; j++)
+		{
+			if (numbers[index] == numbers[j].reverse) {
+
+			}
+		}
+	}
+
+
+}
+
+
+
+
