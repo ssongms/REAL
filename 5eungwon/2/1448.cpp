3bfc4e808586fab17a56356b232dd6f94e2aa614
5eungwon <kimseungwon0630@naver.com>
2024-04-03 20:45:36 +0900

    2주차

--- 5eungwon/2/1448.cpp
+++ 5eungwon/2/1448.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-using namespace std;
+using namespace std; 
 int main() {
 	int n;
 	cin >> n;
 	vector<int>x;
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) {   //벡터에 각 변 넣어줌
 		int a;
 		cin >> a;
 		x.push_back(a);
 	}
-	sort(x.begin(), x.end(), greater<>());
+	sort(x.begin(), x.end(), greater<>());   //내림차순으로  정렬
 	for (int i = 0; i < n - 2; i++) {
-		int q = max({ x[i],x[i + 1],x[i + 2] });
+		int q = max({ x[i],x[i + 1],x[i + 2] });   //0번째 변부터 n-3번 변까지 3개씩 확인해서 가장큰값을 가장 긴변으로 정한 후 삼각형의 조건을 만족하면 합 구한후 
 		int summ = 0;
 		int che = 0;
 		for (int j = i; j < i + 3; j++) {
@@ -23,10 +23,10 @@ int main() {
 			}
 			summ += x[j];
 		}
-		if (q < summ) {
+		if (q < summ) {   //각 구한 합의 최대값 갱신
 			cout << summ + q;
 			return 0;
 		}
 	}
-	cout << -1;
+	cout << -1;   //못만들면 -1출력
 }
--- 5eungwon/2/2847.cpp
+++ 5eungwon/2/2847.cpp
@@ -6,16 +6,16 @@ int main() {
 	cin >> n;
 	int summ = 0;
 	vector<int>box;
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) {   //n번 만큼 게임변수 입력받음
 		int a;
 		cin >> a;
 		box.push_back(a);
 	}
-	for (int i = box.size() - 2; i >= 0; i--) {
+	for (int i = box.size() - 2; i >= 0; i--) {   //벡터의 인덱스의 끝 -1 부터 탐색하여 i의 값이 i+1의 값보다 작아질때까지 감수 -1 
 		while (box[i] >= box[i + 1]) {
-			summ++;
+			summ++;       //횟수를 summ변수에 저장
 			box[i]--;
 		}
 	}
-	cout << summ;
+	cout << summ;   //summ 출력
 }
