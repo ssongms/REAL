cee1e4a13546a7cf18ffc58a01a1a44a1605f9ef
junghyunseok <jhyunseok325@gmail.com>
2024-04-07 11:26:19 +0900

    1448.cpp 코드 수정

--- Junghs21/2/1448.cpp
+++ Junghs21/2/1448.cpp
@@ -10,40 +10,33 @@ int main() {
 	int numberOfStraw;
 	cin >> numberOfStraw;
 
+	//가장 긴 삼격형의 길이를 저장할 maxLength 변수 생성
+	int maxLength = -1;
+
 	//빨대가 3개 미만이면 삼각형 못만드니 코드 종료
 	if (numberOfStraw < 3) {
 		cout << "삼각형 만들기 불가";
 		return 0;
 	}
 
-	//빨대의 수만큼 입력받는 cm 벡터, 길이를 저장할 total 벡터 생성
-	vector<int> cm(numberOfStraw);
-	vector<int> total(numberOfStraw);
+	//빨대의 수만큼 입력받는 cm 벡터 생성
+	vector<int> straws(numberOfStraw);
 
 	//수만큼 입력 받음
 	for (int i = 0; i < numberOfStraw; i++) {
-			cin >> cm[i];
+			cin >> straws[i];
 	}
 
 	//빨대의 길이를 내림차순으로 정렬
-	sort(cm.begin(), cm.end(), greater<int>());
-	total[0] = -1;
+	sort(straws.begin(), straws.end(), greater<int>());
 
-	//빨대로 만든 삼각형의 모든 길이를 구해서 total에 저장
+	//빨대로 만들 수 있는 모든 삼각형의 길이를 구함 
 	for (int i = 0; i < numberOfStraw - 2; ++i) {
-		if (cm[i] < (cm[i+1] + cm[i+2])) {
-			total[i+1] = cm[i] + cm[i + 1] + cm[i + 2];
-		}
-		//갯수가 많아도 맨 처음 3개의 숫자가 위에 if문을 만족하지않으면 바로 끝나는 오류발생
-		else {
-			cout << total[0];
-			return 0;
+		if (straws[i] < (straws[i+1] + straws[i+2])) {
+	//max함수를 통하여 maxLength에 현재 입력되어 있는 가장 큰 길이 값과, 지금 반복문을 통해 만들 수 있는 길이 값중 큰것을 maxLength에 저장
+			maxLength = max(maxLength, straws[i] + straws[i + 1] + straws[i + 2]);
 		}
 	}
 
-	//모든 길이를 저장한 total을 내림차순으로 정렬
-	sort(total.begin(), total.end(), greater<int>());
-
-	//가장 긴 길이 출력
-	cout << total[0];
+	cout << maxLength;
 }
