e4893854c979c20974eac8743bb9c678163f90b2
송민석 <songms0909@khu.ac.kr>
2024-05-09 23:29:04 +0900

    4주차

--- ssongms/4/1912.py
+++ ssongms/4/1912.py
@@ -1,7 +1,7 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-dp = [0] * n
+dp = [0] * n # 누적 합을 기록한 DP 배열
 dp[0] = arr[0]
 for i in range(1, n):
     dp[i] = max(arr[i], dp[i-1] + arr[i]) # 현재 인덱스 i에서 자기 자신(arr[i])과 이전의 누적 합에 자기 자신을 더했을 때(dp[i-1]+arr[i]) 중 더 큰 값을 기록
