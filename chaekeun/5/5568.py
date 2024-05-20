396784129c916c8e258d1d72202a7e60375ec770
chaekeun <chaen328@khu.ac.kr>
2024-05-20 10:14:27 +0900

    5주차

---
+++ chaekeun/5/5568.py
@@ -0,0 +1,19 @@
+from sys import stdin
+from itertools import permutations
+input = stdin.readline
+
+n = int(input())
+k = int(input())
+a = [int(input()) for _ in range(n)]
+cnt = 0
+ans = []
+
+for p in permutations(a, k):
+    temp = ""
+    for elem in p: 
+        temp += str(elem)
+    ans.append(temp)
+
+
+cnt = len(set(ans))
+print(cnt)
